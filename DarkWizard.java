6500d338f05d9f25d191367b64ea9210c5ef4856
NathanielUnger <34070659+NathanielUnger@users.noreply.github.com>
2017-12-05 21:53:56 -0500

    Update DarkWizard.java

--- DarkWizard.java
+++ DarkWizard.java
@@ -1,6 +1,6 @@
 public class DarkWizard extends Monster {
     public DarkWizard() {
-	setHp(75);
+	setHP(75);
 	setStrength((int) Math.random()*45 + 80);
 	setDefense((int) Math.random()*10 + 5);
 	setAttack(1);
